81d162bbb02147ca011a660a91d1aff9375c3eba
TFO\jymele <jymele@tfo.org>
2025-03-25 14:15:04 -0400

    Can now move and change rotation of blocks

--- src/main.ts
+++ src/main.ts
@@ -16,12 +16,17 @@ let then = 0;
 // block position
 let currentRow: number;
 let currentColumn: number;
+let currentRotation: 0 | 1 | 2 | 3;
 let currentBlock: number[];
 
 function reset() {
-  currentRow = 0;
+  currentRow = -1;
   currentColumn = startColumn;
-  currentBlock = Blocks.iPiece(currentRow * config.rows + currentColumn, 0);
+  currentRotation = 0;
+  currentBlock = Blocks.iPiece(
+    currentRow * config.rows + currentColumn,
+    currentRotation
+  );
 
   // cancel animation frame
 }
@@ -36,12 +41,34 @@ function play() {
 
     // update the block
     currentRow += 1;
-    currentBlock = Blocks.iPiece(currentRow * config.cols + currentColumn, 0);
+    currentBlock = Blocks.iPiece(
+      currentRow * config.cols + currentColumn,
+      currentRotation
+    );
   }
 
   requestAnimationFrame(play);
 }
 
 reset();
-
 playButton.addEventListener("click", play);
+
+window.addEventListener("keydown", (e) => {
+  switch (e.code) {
+    case "ArrowLeft":
+      if (currentColumn > 0) currentColumn -= 1;
+      break;
+    case "ArrowRight":
+      if (currentColumn < config.cols - 1) currentColumn += 1;
+      break;
+    case "Space":
+      if (currentRotation < 3) currentRotation += 1;
+      else currentRotation = 0;
+  }
+
+  currentBlock = Blocks.iPiece(
+    currentRow * config.cols + currentColumn,
+    currentRotation
+  );
+  draw(currentBlock);
+});
