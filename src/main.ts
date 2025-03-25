6c952b53d18967a0c5dce061bd79ac9ddb4bf981
TFO\jymele <jymele@tfo.org>
2025-03-25 13:59:02 -0400

    The blocks come down

--- src/main.ts
+++ src/main.ts
@@ -10,12 +10,38 @@ initialize();
 
 const startColumn = Math.floor(config.cols / 2) - 1;
 
-let currentBlock = Blocks.iPiece(startColumn, 0);
+let now = 0;
+let then = 0;
+
+// block position
+let currentRow: number;
+let currentColumn: number;
+let currentBlock: number[];
+
+function reset() {
+  currentRow = 0;
+  currentColumn = startColumn;
+  currentBlock = Blocks.iPiece(currentRow * config.rows + currentColumn, 0);
+
+  // cancel animation frame
+}
 
 function play() {
+  then = Date.now();
   clearBoard();
   draw(currentBlock);
+
+  if (Date.now() - now > 1000) {
+    now = then;
+
+    // update the block
+    currentRow += 1;
+    currentBlock = Blocks.iPiece(currentRow * config.cols + currentColumn, 0);
+  }
+
   requestAnimationFrame(play);
 }
+
+reset();
 
 playButton.addEventListener("click", play);
